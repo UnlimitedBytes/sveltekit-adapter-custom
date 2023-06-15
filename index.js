2fa086b6fae403c8059988beac79b4694228b31b
UnlimitedBytes <admin@unlimitedbytes.ovh>
2023-06-15 22:09:39 +0200

    feat: add hook to build output

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-// @ts-ignore
 import { fileURLToPath } from 'url';
 import fs from 'node:fs';
-import path from 'node:path';
 import { rollup } from 'rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 
 const files = fileURLToPath(new URL('./files', import.meta.url).href);
+const hookContent = `
+import { get_hooks } from './internal.js';
+
+async function initHook(httpServer) {
+    const hooks = await get_hooks();
+    const handler = hooks['setup'];
+    handler(httpServer);
+}
+
+export { initHook };
+`;
 
 /** @type {import('.').default} */
 export default function (options = {}) {
-    // @ts-ignore
     const { out = 'build', precompress, envPrefix = '', polyfill = true } = options;
 
     return {
@@ -20,109 +28,96 @@ export default function (options = {}) {
         async adapt(builder) {
             const tmp = builder.getBuildDirectory('adapter-node');
 
-            // TODO: extract all of this blocks in own functions
-            {
-                builder.rimraf(out);
-                builder.rimraf(tmp);
-                builder.mkdirp(tmp);
-            }
+            // cleanup
+            builder.rimraf(out);
+            builder.rimraf(tmp);
+            builder.mkdirp(tmp);
 
-            {
-                builder.log.minor('Copying assets..');
-                builder.writeClient(`${out}/client${builder.config.kit.paths.base}`);
-                builder.writePrerendered(`${out}/prerendered${builder.config.kit.paths.base}`);
-            }
+            // copy assets
+            builder.log.minor('Copying assets..');
+            builder.writeClient(`${out}/client${builder.config.kit.paths.base}`);
+            builder.writePrerendered(`${out}/prerendered${builder.config.kit.paths.base}`);
 
+            // precompress
             if (precompress) {
                 builder.log.minor('Compressing assets');
                 const jobs = [builder.compress(`${out}/client`), builder.compress(`${out}/prerendered`)];
                 await Promise.all(jobs);
             }
 
-            {
-                builder.log.minor('Generating manifest..');
-                let manifest = `export const manifest = ${builder.generateManifest({ relativePath: './' })};\n\n`;
-                manifest += `export const prerendered = new Set(${JSON.stringify(builder.prerendered.paths)});\n`;
-                fs.writeFileSync(`${tmp}/manifest.js`, manifest);
-            }
+            // copy sveltekit server
+            builder.log.minor('Copying server');
+            builder.writeServer(tmp);
 
-            {
-                builder.log.minor('Building server');
-                builder.writeServer(tmp);
+            // generate manifest
+            builder.log.minor('Generating manifest..');
+            let manifest = `export const manifest = ${builder.generateManifest({ relativePath: './' })};\n\n`;
+            manifest += `export const prerendered = new Set(${JSON.stringify(builder.prerendered.paths)});\n`;
+            fs.writeFileSync(`${tmp}/manifest.js`, manifest);
 
-                const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-                const bundle = await rollup({
-                    input: {
-                        index: `${tmp}/index.js`,
-                        manifest: `${tmp}/manifest.js`,
-                    },
-                    external: [
-                        // dependencies could have deep exports, so we need a regex
-                        ...Object.keys(pkg.dependencies || {}).map((d) => new RegExp(`^${d}(\\/.*)?$`)),
-                    ],
-                    plugins: [
-                        nodeResolve({
-                            preferBuiltins: true,
-                            exportConditions: ['node'],
-                        }),
-                        commonjs({ strictRequires: true }),
-                        json(),
-                    ],
-                });
+            // generate hook file
+            builder.log.minor('Generating hook..');
+            fs.writeFileSync(`${tmp}/hook.js`, hookContent);
 
-                await bundle.write({
-                    dir: `${out}/server`,
-                    format: 'esm',
-                    sourcemap: true,
-                    chunkFileNames: 'chunks/[name]-[hash].js',
-                });
+            // build sveltekit server
+            builder.log.minor('Building server');
+            const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+            const bundle = await rollup({
+                input: {
+                    index: `${tmp}/index.js`,
+                    manifest: `${tmp}/manifest.js`,
+                    hook: `${tmp}/hook.js`,
+                },
+                external: [
+                    // dependencies could have deep exports, so we need a regex
+                    ...Object.keys(pkg.dependencies || {}).map((d) => new RegExp(`^${d}(\\/.*)?$`)),
+                ],
+                plugins: [
+                    nodeResolve({
+                        preferBuiltins: true,
+                        exportConditions: ['node'],
+                    }),
+                    commonjs({ strictRequires: true }),
+                    json(),
+                ],
+            });
 
-                builder.copy(files, out, {
-                    replace: {
-                        ENV: './env.js',
-                        HANDLER: './handler.js',
-                        MANIFEST: './server/manifest.js',
-                        SERVER: './server/index.js',
-                        SHIMS: './shims.js',
-                        ENV_PREFIX: JSON.stringify(envPrefix),
-                    },
-                });
-            }
+            await bundle.write({
+                dir: `${out}/server`,
+                format: 'esm',
+                sourcemap: true,
+                chunkFileNames: 'chunks/[name]-[hash].js',
+            });
+
+            // build node server
+            builder.copy(files, out, {
+                replace: {
+                    ENV: './env.js',
+                    HANDLER: './handler.js',
+                    MANIFEST: './server/manifest.js',
+                    SERVER: './server/index.js',
+                    SHIMS: './shims.js',
+                    HOOK: './server/hook.js',
+                    ENV_PREFIX: JSON.stringify(envPrefix),
+                },
+            });
 
             // If polyfills aren't wanted then clear the file
             if (!polyfill) {
                 fs.writeFileSync(`${out}/shims.js`, '', 'utf-8');
             }
 
-            // TODO: extract this block in it's own function too
+            // copy dependencies
             builder.log.minor('Copying dependencies..');
-            let packageJson = null;
-            try {
-                const packageJsonContent = fs.readFileSync(path.resolve('./package.json'), {
-                    encoding: 'utf-8',
-                });
-                packageJson = JSON.parse(packageJsonContent);
-            } catch (
-                /** @type {any} */
-                error
-            ) {
-                builder.log.error("Couldn't load package.json from your sveltekit project.");
-                builder.log.error(error);
-                return;
-            }
-
             fs.writeFileSync(
                 `${out}/package.json`,
                 JSON.stringify({
-                    ...packageJson,
-                    private: packageJson.private ?? true,
-                    type: packageJson.type ?? 'module',
-                    name: packageJson.name || 'sveltekit-custom-server',
-                    version: packageJson.version || '1.0.0',
-                    main: 'index.js',
-                    dependencies: {
-                        ...(packageJson.dependencies || {}),
-                    },
+                    ...pkg,
+                    private: pkg.private ?? true,
+                    type: pkg.type ?? 'module',
+                    name: pkg.name || 'sveltekit-custom-server',
+                    version: pkg.version || '1.0.0',
+                    main: pkg.main || 'index.js',
                 })
             );
         },
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
+import http from 'http';
 import polka from 'polka';
 // @ts-expect-error
 import { handler } from 'HANDLER';
 // @ts-expect-error
 import { env } from 'ENV';
+// @ts-expect-error
+import { initHook } from 'HOOK';
 
 export const path = env('SOCKET_PATH', false);
 export const host = env('HOST', '0.0.0.0');
 export const port = env('PORT', !path && '3000');
 
-const server = polka().use(handler);
+const httpServer = http.createServer();
+const server = polka({ server: httpServer }).use(handler);
 
+await initHook(httpServer);
 server.listen({ path, host, port }, () => {
     console.log(`Listening on ${path ? path : host + ':' + port}`);
 });
